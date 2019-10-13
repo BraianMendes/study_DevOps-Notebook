9cdfd3591dd7d35499ace473134e9b3065adc27a
Braian Corrêa <braian.unifei@gmail.com>
2019-10-12 21:55:42 -0300

    12/10/2019

---
+++ Deep Study in language/Python2-Study/Codecademy Course - Learn Python 2/Conditional & Control Flow/bolleans pt8.py
@@ -0,0 +1,22 @@
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = True and True
+
+# Make me false!
+bool_three = False or False
+
+# Make me true!
+bool_four = not False or False
+
+# Make me true!
+bool_five = True and (True or not True)
+
+print bool_one
+print bool_two
+print bool_three
+print bool_four
+print bool_five
