a87ed39d57cbc58455ed29e3ea31e166158c817c
Braian Corrêa <braian.unifei@gmail.com>
2019-10-18 06:06:26 -0300

    18/10/2019

---
+++ Deep Study in language/Go/Codecademy Course - Learn Go/Introduction/Ascii art.go
@@ -0,0 +1,18 @@
+package main
+
+import f "fmt"
+
+func main () {
+f.Println("      `.-::::::-.`")
+f.Println("  .:-::::::::::::::-:.")
+f.Println("  `_:::    ::    :::_`")
+f.Println("   .:( ^   :: ^   ):.")
+f.Println("   `:::   (..)   :::.")
+f.Println("   `:::::::UU:::::::`")
+f.Println("   .::::::::::::::::.")
+f.Println("   O::::::::::::::::O")
+f.Println("   -::::::::::::::::-")
+f.Println("   `::::::::::::::::`")
+f.Println("    .::::::::::::::.")
+f.Println("      oO:::::::Oo    ")
+}
---
+++ Deep Study in language/Go/Codecademy Course - Learn Go/Introduction/Comments.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  //Are we racing or coding?
+	/*fmt.Println("Ready")
+  fmt.Println("Set")*/
+  fmt.Println("Gooooo!")
+}
