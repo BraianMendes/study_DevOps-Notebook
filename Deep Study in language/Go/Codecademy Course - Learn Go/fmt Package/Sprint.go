6471650a10b410c8cf20819caaf2396053fa2051
Braian Corrêa <braian.unifei@gmail.com>
2019-10-30 00:51:03 -0300

    Go - fmt

---
+++ Deep Study in language/Go/Codecademy Course - Learn Go/fmt Package/Sprint.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  step1 := "Breathe in..."
+  step2 := "Breathe out..."
+  
+  // Add your code below:
+  meditation := fmt.Sprintln(step1, step2)
+  
+  fmt.Print(meditation)
+}
