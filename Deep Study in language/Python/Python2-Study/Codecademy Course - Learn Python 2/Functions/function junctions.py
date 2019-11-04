19105e986a2098dfc021d0a2cad7fe69942ed59c
Braian Corrêa <braian.unifei@gmail.com>
2019-11-03 23:07:59 -0300

    03/11/2019

---
+++ Deep Study in language/Python/Python2-Study/Codecademy Course - Learn Python 2/Functions/function junctions.py
@@ -0,0 +1,10 @@
+# Define your spam function starting on line 5. You
+# can leave the code on line 10 alone for now--we'll
+# explain it soon!
+
+def spam():
+  """Prints 'Eggs!' to the console."""
+  print "Eggs!"
+  
+# Define the spam function above this line.
+spam()
